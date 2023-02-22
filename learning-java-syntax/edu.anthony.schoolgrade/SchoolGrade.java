b2710f44afdfa64b16309c2829853d6402e5fce3
Anthhon <anthcomercial@gmail.com>
2023-02-22 17:23:06 -0300

    learning syntax initial files added

---
+++ learning-java-syntax/edu.anthony.schoolgrade/SchoolGrade.java
@@ -0,0 +1,11 @@
+public class SchoolGrade{
+	public static void main(String[] args){
+		int finalScore = 7,
+		    requiredScore = 7;
+
+		if (finalScore < requiredScore)
+			System.out.println("REPROVED");
+		else
+			System.out.println("APPROVED");
+	}
+}
