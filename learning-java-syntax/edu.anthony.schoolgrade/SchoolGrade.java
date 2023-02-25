34738f534f037adecc2ba2d1f4dc5ded55e65434
Anthhon <anthcomercial@gmail.com>
2023-02-25 10:31:04 -0300

    code small changes

--- learning-java-syntax/edu.anthony.schoolgrade/SchoolGrade.java
+++ learning-java-syntax/edu.anthony.schoolgrade/SchoolGrade.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class SchoolGrade{
 	public static void main(String[] args){
-		int finalScore = 7,
-		    requiredScore = 7;
+		// Creates a scanner to tokenize the user inputs
+		Scanner scan = new Scanner(System.in);
+		int score,
+		    minScore = 7;
 
-		if (finalScore < requiredScore)
-			System.out.println("REPROVED");
-		else
-			System.out.println("APPROVED");
+		// Asks for a valid input
+		System.out.println("Type a score beetween 0 and 10");
+		System.out.print("Score: ");
+		score = scan.nextInt();
+
+		// Avoid mystaken scores
+		while(score < 0 || score > 10){
+			System.out.println("Invalid score, try again!");
+			System.out.print("Score: ");
+			score = scan.nextInt();
+		}
+
+		System.out.println("Congratulations! The score was updated sucessfully!");
 	}
 }
